bea22f70e677610c351c4078f6f05c53845930dd
zakumann <samuel742able@gmail.com>
2022-09-10 00:47:37 +0900

    upload news-viewer

---
+++ news-viewer/src/App.js
@@ -0,0 +1,17 @@
+import { useState, useCallback } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
+const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
+
+  return(
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
+};
+
+export default App;
