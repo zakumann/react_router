559c3b4b91719e0a39cff81a83cfdf222bd44c2a
zakumann <samuel742able@gmail.com>
2022-10-01 11:45:42 +0900

    update react-redux-tutorial 10012022

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,19 +1,18 @@
-import { connect } from 'react-redux';
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-    return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-    );
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+    return <Counter
+        number={number}
+        onIncrease={onIncrease}
+        onDecrease={onDecrease}
+    />;
 };
 
-export default connect(
-    state=> ({
-        number: state.counter.number,
-    }),
-        {
-            increase,
-            decrease,
-        },
-)(CounterContainer);
+export default CounterContainer;
