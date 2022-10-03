e632e47ca35cac0878d52be99eb656370d330f2f
zakumann <samuel742able@gmail.com>
2022-10-04 00:27:36 +0900

    update react-redux-middleware

---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider >,
+  document.getElementById('root')
+);
