26d6e14e0f978cf5f83c07041da4cb5d188073ff
zakumann <samuel742able@gmail.com>
2022-10-04 20:15:27 +0900

    update 10042022

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux';
-import { increase, decrease } from '../modules/counter';
+import { increaseAsync, decreaseAsync } from '../modules/counter';
 import Counter from '../components/Counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = ({ number, increaseAsync, decreaseAsync }) => {
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+        <Counter 
+            number={number} 
+            onIncrease={increaseAsync} 
+            onDecrease={decreaseAsync} 
+        />
     );
 };
 
@@ -13,7 +17,7 @@ export default connect(
         number: state.counter
     }),
     {
-        increase,
-        decrease
+        increaseAsync,
+        decreaseAsync
     }
 )(CounterContainer);
--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+//import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
