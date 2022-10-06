96b8c2aa93160b78095e3bae0bae74080e0ee19e
zakumann <samuel742able@gmail.com>
2022-10-06 13:01:54 +0900

    Update Redux-middleware-saga

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,9 +1,9 @@
-import SampleContainer from './containers/SampleContainer';
+import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
   return (
     <div>
-      <SampleContainer />
+      <CounterContainer/>
     </div>
   );
 };
