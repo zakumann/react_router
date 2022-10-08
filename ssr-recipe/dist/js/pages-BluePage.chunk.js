a2d7c4cc538181335f593353c9559c9b177a6dce
zakumann <samuel742able@gmail.com>
2022-10-08 19:17:50 +0900

    Commit before eject

---
+++ ssr-recipe/dist/js/pages-BluePage.chunk.js
@@ -0,0 +1,1 @@
+exports.ids=[1],exports.modules={22:function(e,t,r){"use strict";r.r(t);var s=r(0),u=(r(3),function(){return Object(s.jsx)("div",{className:"Blue",children:"Blue"})});t.default=function(){return Object(s.jsx)(u,{})}}};
