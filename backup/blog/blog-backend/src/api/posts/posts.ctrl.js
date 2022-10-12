ad8b45df0ec4fe2e07b5a7a75f177b500812a20e
zakumann <samuel742able@gmail.com>
2022-10-12 14:08:07 +0900

    update 10122022

--- backup/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ backup/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -4,10 +4,30 @@ import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
     const { id } = ctx.params;
     if (!ObjectId.isValid(id)) {
         ctx.status = 400; // Bad Request
+        return;
+    }
+    try {
+      const post = await Post.findById(id);
+      // 포스트가 존재하지 않을 때
+      if (!post) {
+        ctx.status = 404; // Not Found
+        return;
+      }
+      ctx.state.post = post;
+      return next();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+  };
+
+  export const checkOwnPost = (ctx, next) => {
+    const { user, post } = ctx.state;
+    if (post.user._id.toString() !== user._id) {
+        ctx.status = 403;
         return;
     }
     return next();
@@ -44,6 +64,7 @@ export const write = async ctx => {
         title,
         body,
         tags,
+        user: ctx.state.user,
     });
     try {
         await post.save();
@@ -52,8 +73,9 @@ export const write = async ctx => {
         ctx.throw(500, e);
     }
 };
+
 /*
-GET /api/posts
+GET /api/posts?username=&tag=&page=
 */
 export const list = async ctx => {
     // query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
@@ -65,14 +87,21 @@ export const list = async ctx => {
         return;
     }
 
+    const { tag, username } = ctx.query;
+    // tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
+    const query = {
+        ...(username ? { 'user.username': username } : {}),
+        ...(tag ? { tags: tag } : {}),
+    };
+
     try {
-        const posts = await Post.find()
+        const posts = await Post.find(query)
         .sort({ _id: -1 })
         .limit(10)
         .skip((page -1)*10)
         .lean()
         .exec();
-        const postCount = await Post.countDocuments().exec();
+        const postCount = await Post.countDocuments(query).exec();
         ctx.set(`Last-Page`, Math.ceil(postCount / 10));
         ctx.body = posts.map(post => ({
             ...post,
@@ -83,22 +112,14 @@ export const list = async ctx => {
         ctx.throw(500, 0);
     }
 };
+
 /*
 GET /api/posts/:id
 */
 export const read = async ctx => {
-    const { id } = ctx.params;
-    try {
-        const post = await Post.findById(id).exec();
-        if (!post){
-            ctx.status = 404; // Not Found
-            return;
-        }
-        ctx.body = post;
-    } catch(e) {
-        ctx.throw(500, 0);
-    }
+    ctx.body = ctx.state.post;
 };
+
 /*
 DELETE /api/posts/:id
 */
@@ -111,6 +132,7 @@ export const remove = async ctx => {
         ctx.throw(500,0);
     }
 };
+
 /*
 PATCH /api/posts/:id
 {
