c1a93121c1a26b6a18e1baf6a762ebf9d4627904
zakumann <samuel742able@gmail.com>
2022-10-14 21:49:09 +0900

    EditorContainer 만들기

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import TagBox from '../components/write/TagBox';
+import WriteActionButton from '../components/write/WriteActionButtons';
+import Responsive from '../components/common/Responsive';
+import EditorContainer from '../containers/write/EditorContainer';
 
 const WritePage = () => {
-  return <div>글쓰기</div>;
+  return (
+    <Responsive>
+      <EditorContainer />
+      <TagBox />
+      <WriteActionButton />
+    </Responsive>
+  );
 };
 
 export default WritePage;
