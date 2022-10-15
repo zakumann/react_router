e1c7acacd83e29e2f31ed3a51c2de8a5c146108c
zakumann <samuel742able@gmail.com>
2022-10-16 01:18:43 +0900

    update latest version

--- blog/blog-frontend/src/components/write/Editor.js
+++ blog/blog-frontend/src/components/write/Editor.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.bubble.css';
 import styled from 'styled-components';
@@ -11,7 +10,6 @@ const EditorBlock = styled(Responsive)`
   padding-top: 5rem;
   padding-bottom: 5rem;
 `;
-
 const TitleInput = styled.input`
   font-size: 3rem;
   outline: none;
@@ -21,7 +19,6 @@ const TitleInput = styled.input`
   margin-bottom: 2rem;
   width: 100%;
 `;
-
 const QuillWrapper = styled.div`
   /* 최소 크기 지정 및 padding 제거 */
   .ql-editor {
@@ -36,24 +33,24 @@ const QuillWrapper = styled.div`
 `;
 
 const Editor = ({ title, body, onChangeField }) => {
-    const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
-    const quillInstance = useRef(null); // Quill 인스턴스를 설정
+  const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
+  const quillInstance = useRef(null); // Quill 인스턴스를 설정
 
-    useEffect(() => {
-        quillInstance.current = new Quill(quillElement.current, {
-            theme: 'bubble',
-            placeholder: '내용을 작성하세요...',
-            modules: {
-                // 더 많은 옵션
-                // https://quilljs.com/docs/modules/toolbar/ 참고
-                toolbar: [
-                    [{ header: '1' }, { header: '2' }],
-                    ['bold', 'italic', 'underline', 'strike'],
-                    [{ list: 'ordered' }, { list: 'bullet' }],
-                    ['blockquote', 'code-block', 'link', 'image'],
-                  ],
-            },
-      });
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: 'bubble',
+      placeholder: '내용을 작성하세요...',
+      modules: {
+        // 더 많은 옵션
+        // https://quilljs.com/docs/modules/toolbar/ 참고
+        toolbar: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['blockquote', 'code-block', 'link', 'image'],
+        ],
+      },
+    });
 
     // quill에 text-change 이벤트 핸들러 등록
     // 참고: https://quilljs.com/docs/api/#events
@@ -69,18 +66,18 @@ const Editor = ({ title, body, onChangeField }) => {
     onChangeField({ key: 'title', value: e.target.value });
   };
 
-    return (
-        <EditorBlock>
-          <TitleInput
-          placeholder="제목을 입력하세요"
-          onChange={onChangeTitle}
-          value={title}
-          />
-          <QuillWrapper>
-            <div ref={quillElement} />
-          </QuillWrapper>
-        </EditorBlock>
-    );
+  return (
+    <EditorBlock>
+      <TitleInput
+        placeholder="제목을 입력하세요"
+        onChange={onChangeTitle}
+        value={title}
+      />
+      <QuillWrapper>
+        <div ref={quillElement} />
+      </QuillWrapper>
+    </EditorBlock>
+  );
 };
 
 export default Editor;
--- blog/blog-frontend/src/containers/write/TagBoxContainer.js
+++ blog/blog-frontend/src/containers/write/TagBoxContainer.js
@@ -4,19 +4,19 @@ import TagBox from '../../components/write/TagBox';
 import { changeField } from '../../modules/write';
 
 const TagBoxContainer = () => {
-    const dispatch = useDispatch();
-    const tags = useSelector(state => state.write.tags);
+  const dispatch = useDispatch();
+  const tags = useSelector(state => state.write.tags);
 
-    const onChangeTags = nextTags => {
-        dispatch(
-            changeField({
-                key: 'tags',
-                value: nextTags,
-            }),
-        );
-    };
+  const onChangeTags = nextTags => {
+    dispatch(
+      changeField({
+        key: 'tags',
+        value: nextTags,
+      }),
+    );
+  };
 
-    return <TagBox onChangeTags={onChangeTags} tags={tags} />;
+  return <TagBox onChangeTags={onChangeTags} tags={tags} />;
 };
 
 export default TagBoxContainer;
